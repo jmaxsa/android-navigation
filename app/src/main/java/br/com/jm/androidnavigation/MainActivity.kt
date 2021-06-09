cf65b5519249d1f3533a6aba4d04ad6552adb1c6
Jonathan Andrade <jonathanmaxuel.ti@gmail.com>
2021-06-09 18:54:27 -0300

    Creating project

---
+++ app/src/main/java/br/com/jm/androidnavigation/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.com.jm.androidnavigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
